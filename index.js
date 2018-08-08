2d7885fdd289f248ceba2555eb07dcd4e721543a
jakecrump <crumps589@gmail.com>
2018-08-08 16:32:04 -0400

    .

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const fs = require('fs')
 const request = require('request')
 const inquirer = require('inquirer')
+const cheerio = require('cheerio')
 
 
 const questions = [
@@ -14,11 +15,24 @@ const questions = [
 	{
 		type:'input',
 		name:'tag',
-		message:"What kind of tag do you want?"
+		message:"What kind of html tag do you want?"
 	},
 	{
 		type:'list',
-		name:'save'
+		name:'info',
+		message:'What kind of data do you want?',
+		choices:[
+			'html',
+			'text',
+			'href',
+			'src',
+			'class',
+			'id'
+		]
+	},
+	{
+		type:'list',
+		name:'save',
 		message:'Would you like to save the results to your desktop?',
 		choices:[
 			'yes',
@@ -29,7 +43,31 @@ const questions = [
 
 
 const answering = (answers)=>{
-	console.log(answers)
+	request(answers.url, (err, res, body)=>{
+		const $ = cheerio.load(body);
+		$(answers.tag).each((i, obj)=>{
+			switch (answers.info){
+				case 'html':
+					console.log($.html(obj))
+					break;
+				case 'text':
+					console.log($(obj).text())
+					break;
+				case 'href':
+					console.log($(obj).attr('href'))
+					break;
+				case 'src':
+					console.log($(obj).attr('src'))
+					break;
+				case 'class':
+					console.log($(obj).attr('class'))
+					break;
+				case 'id':
+					console.log($(obj).attr('id'))
+					break;
+			}
+		})
+	})
 }
 
 
