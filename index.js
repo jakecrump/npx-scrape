2df6882d4937060a9ce625fe4b5088e0f1e74730
jakecrump <crumps589@gmail.com>
2018-08-08 16:15:46 -0400

    .

---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const request = require('request')
+const inquirer = require('inquirer')
+
+
+const questions = [
+	{
+		type:'input',
+		name:'url',
+		message: "Enter url to scrape"
+	},
+	{
+		type:'input',
+		name:'tag',
+		message:"What kind of tag do you want?"
+	}
+	{
+		type:'list',
+		name:'save'
+		message:'Would you like to save the results to your desktop?',
+		choices:[
+			'yes',
+			'no'
+		]
+	}
+
+]
+
+
+const answering = (answers)=>{
+	console.log(answers)
+}
+
+
+inquirer.prompt(questions)
+  .then(answers => {
+  	answering(answers)
+  });
+
+
+
+
